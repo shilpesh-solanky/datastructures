824c739a972f3722df91d0b6397ebb52f53e16a0
Shilpesh Solanky <shilpesh.solanky@netcracker.com>
2017-02-03 15:06:53 +0530

    Restructuring

---
+++ src/Data Structure Problems/Array/Product_Exclude_Index_Example.java
@@ -0,0 +1,15 @@
+
+public class Product_Exclude_Index_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int[] productExcludeIndex(int[] intArray){
+		
+		return null;
+		
+	}
+
+}
