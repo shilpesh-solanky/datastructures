145496782343936a64b435fdf2ae387f3c0f5578
shilpesh <shilsolanky@gmail.com>
2017-02-08 11:05:01 +0530

    Adding some APIS

---
+++ src/datastructure/string/FindAllPermutations.java
@@ -0,0 +1,29 @@
+package datastructure.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by SHILPESH on 08-Feb-17.
+ */
+public class FindAllPermutations {
+
+    public static void main(String[] args) {
+        String str = "abc";
+        System.out.print("Input  " + str + "  Result  " + findAllPermutations(str));
+    }
+
+    public static List<String> findAllPermutations(String inputString) {
+        List<String> result = new ArrayList<>();
+        result.add(inputString);
+        StringBuffer sb = new StringBuffer(inputString);
+
+        for (int i = 0; i < sb.length() - 1; i++) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(i + 1));
+            sb.setCharAt(i + 1, temp);
+            result.add(sb.toString());
+        }
+        return result;
+    }
+}
---
+++ src/datastructure/string/ReverseString.java
@@ -0,0 +1,7 @@
+package datastructure.string;
+
+/**
+ * Created by SHILPESH on 08-Feb-17.
+ */
+public class ReverseString {
+}
