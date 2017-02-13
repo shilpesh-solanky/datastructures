0c342f1431b81b86141fab634df9a59cda08274e
shilpesh <shilsolanky@gmail.com>
2017-02-13 10:30:21 +0530

    Adding Testcase and code of Bubble Sort

--- Algorithms/src/main/java/sorting/BubbleSort.java
+++ Algorithms/src/main/java/sorting/BubbleSort.java
@@ -1,30 +1,49 @@
 package sorting;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Created by SHILPESH on 12-Feb-17.
  */
 public class BubbleSort {
 
-    public List sort(List list){
-        List ret = new ArrayList();
+    public List<Integer> sort(List<Integer> list){
         int size = list.size();
 
         for (int i=0;i<size;i++){
 
+            for (int j=i;j<size;j++){
+
+                if(list.get(i)>list.get(j)){
+
+                    Integer temp=list.get(i);
+                    list.set(i,list.get(j));
+                    list.set(j,temp);
+
+                }
+
+            }
 
         }
 
-        return ret;
+        return list;
     }
 
 
     @Test
     public void testSort(){
-        System.out.println("First Test");
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(3);
+        list.add(9);
+        list.add(2);
+        System.out.println(this.sort(list));
+        //TODO Complete Tests
+      //  Assert.assertArrayEquals();
     }
 }
